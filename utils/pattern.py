1183a95123007957c5ac5034cc76332822a74be5
nicholaszako <nicholas.zako@hotmail.com>
2026-05-30 13:32:11 -0400

    Add multithreading

--- utils/pattern.py
+++ utils/pattern.py
@@ -1,6 +1,7 @@
 import json
 import time
 from pathlib import Path
+from threading import Thread, Event
 
 from api import Api
 
@@ -10,6 +11,8 @@ class Pattern:
         self.pattern_json = pattern_json
         self.api = api_client
         self.name = name
+        self.thread = None
+        self._stop_event = Event()
         if not self.validate():
             raise Exception('Invalid pattern')
         
@@ -17,15 +20,29 @@ class Pattern:
         # STUB
         return True
     
-    # Bear in mind this is (currently) not asynchronous
     def start(self):
+        if self.thread:
+            self.stop()
+        self._stop_event.clear()
+        self.thread = Thread(target=self._send)
+        self.thread.start()
+
+    def stop(self):
+        self._stop_event.set()
+    
+    # Returns true iff entire pattern was sent successfully
+    def _send(self):
         c = self.api.control
-        for step in self.pattern_json:
-            request_args = step['request']
-            sleep_s = step['sleep']/1000
+        for msg in self.pattern_json:
+            if self._stop_event.is_set():
+                # Stop before attempting to send message
+                return False
+            request_args = msg['request']
+            sleep_s = msg['sleep']/1000
             prepped = c.get_prepared_req(**request_args)
             c.send(prepped)
             time.sleep(sleep_s)
+        return True
             
 
 def get_pattern_from_file(path: Path, api_client: Api, 
